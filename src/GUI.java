6d1ca5a8ce3d24c9353d0053c545e0f17c4fe4d2
Lucas Wilbur <lucaswilbur@Lucass-MacBook-Pro.local>
2021-07-28 17:40:46 -0400

    Created IO file for save/load, half-finished implementation into GUI.

--- src/GUI.java
+++ src/GUI.java
@@ -1,13 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
 
 /**
  * Contains the two panes, handles Swing window creation and presentation of
  * panes
  */
-public class GUI extends JFrame implements KeyListener {
+public class GUI extends JFrame implements KeyListener, ActionListener {
     RawPane rawPane;            // Holds raw text
     PreviewPane previewPane;    // Holds preview text
     JTextArea test;             // TODO: delete this
@@ -24,11 +27,25 @@ public class GUI extends JFrame implements KeyListener {
      * Set window traits
      */
     private void setupGUI() {
+        // Determining window traits
         setTitle("Mark It Down");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Quit when GUI closes
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setPreferredSize(screenSize);                   // Fit to screen
         setLayout(new GridLayout(1, 2));
+
+        // Creating menu bar entries
+        System.setProperty("apple.laf.useScreenMenuBar", "true"); // only works on macs?
+        JMenuBar bar = new JMenuBar();
+        JMenu file = new JMenu("File");
+        JMenuItem save = new JMenuItem("Save");
+        JMenuItem load = new JMenuItem("Load");
+        save.addActionListener(this);
+        load.addActionListener(this);
+        file.add(save);
+        file.add(load);
+        bar.add(file);
+        setJMenuBar(bar);
     }
 
     /**
@@ -50,6 +67,27 @@ public class GUI extends JFrame implements KeyListener {
         //previewPane.setText(rawString);  // TODO: uncomment preview, remove test
         test.setText(rawString);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser fc = new JFileChooser();
+
+        String action = e.getActionCommand();
+
+
+        switch (action) {
+            case "Save":  // TODO: make only .txt selectable
+                if (fc.showDialog(this, "Save") == JFileChooser.APPROVE_OPTION) {
+                    IO.writeTextFile(fc.getSelectedFile().getPath(),
+                                     test.getText()); // TODO: change to preview pane
+                }
+                break;
+            case "Load":  // TODO: finish loading process
+                System.out.println("Load " + action + "activated");
+                break;
+        }
+    }
+
 
     @Override
     public void keyReleased(KeyEvent e) {
